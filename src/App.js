db35ffb7b019672e33c6286f02a4ab54f36a21a6
surajpshenoy <surajpshenoy@gmail.com>
2021-08-04 16:16:39 +1000

    css added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import React from "react"
-import text from "./data"
+import React, { useState } from "react";
+import text from "./data";
 function App() {
+  const [count, setCount] = useState(0);
+  const [text, setText] = useState([]);
   return (
-<div>
-<h4>RemSum Generator</h4>
-{console.log(text)}
-</div>
+    <div>
+      <h4>RemSum Generator</h4>
+      {console.log(text)}
+    </div>
   );
 }
 
