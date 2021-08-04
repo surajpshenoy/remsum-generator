7bc325952ce6a4ef49e0c9d64c7cf777eb386f7d
surajpshenoy <surajpshenoy@gmail.com>
2021-08-04 16:28:24 +1000

    Form structure added

--- src/App.js
+++ src/App.js
@@ -3,11 +3,27 @@ import text from "./data";
 function App() {
   const [count, setCount] = useState(0);
   const [text, setText] = useState([]);
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+    console.log('Function invoked')
+  }
+
   return (
-    <div>
-      <h4>RemSum Generator</h4>
-      {console.log(text)}
-    </div>
+    <section className="section-center">
+      <h3>RemSem Text Generator</h3>
+      <form className="lorem-form" onSubmit={submitHandler}>
+      <label  htmlFor="amount">
+        Paragraphs: 
+      </label>
+      <input type="text" name="amount" id="amount" value={count} onChange={(e)=> {setCount(e.target.value)}}></input>
+      </form>
+      <button type="submit" className="btn">generate</button>
+      <article className="lorem-text">
+        <p> Generated text here
+        </p>
+      </article>
+    </section>
   );
 }
 
